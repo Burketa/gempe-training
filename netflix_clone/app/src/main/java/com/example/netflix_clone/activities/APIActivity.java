993345cc3bb24bfc23c7277cd902525d81f64d5f
burca <rlpelizzon@hotmail.com>
2019-05-31 18:23:01 -0300

    coemcando a iteracao do json retornado para exibir os resultados

--- netflix_clone/app/src/main/java/com/example/netflix_clone/activities/APIActivity.java
+++ netflix_clone/app/src/main/java/com/example/netflix_clone/activities/APIActivity.java
@@ -7,10 +7,10 @@ import android.support.v7.widget.DividerItemDecoration;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.widget.LinearLayout;
-import android.widget.Toast;
 
 import com.example.netflix_clone.R;
 import com.example.netflix_clone.adapters.AdapterFeedbacks;
+import com.example.netflix_clone.model.Feedback;
 
 import org.json.JSONException;
 import org.json.JSONObject;
@@ -23,15 +23,18 @@ import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 
 public class APIActivity extends AppCompatActivity {
 
     private StringBuffer api_raw_result;
 
-    private List<AdapterFeedbacks> api_result;
+    private List<Feedback> api_result;
 
     private RecyclerView recyclerView;
+
+    private AdapterFeedbacks adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -40,9 +43,6 @@ public class APIActivity extends AppCompatActivity {
 
         api_result = new ArrayList<>();
         api_raw_result = new StringBuffer();
-
-        //Referencias
-        recyclerView = findViewById(R.id.recycler_view);
 
         //Task
         startTask();
@@ -107,30 +107,42 @@ public class APIActivity extends AppCompatActivity {
             String buy_price;
             String sell_price;
 
+
             //TODO: Iterar em cada moeda para colocar os valores em uma lista para colocar no adapter
             try {
-                JSONObject jsonObjectCurrencys = new JSONObject(result);
-                currency = jsonObjectCurrencys.getString("ITERACAO EM CADA MOEDA");
 
-                JSONObject jsonObjectSelectedCurrency = new JSONObject(result);
-                buy_price = jsonObjectSelectedCurrency.getString("buy");
-                buy_price = jsonObjectSelectedCurrency.getString("sell");
+                JSONObject issueObj = new JSONObject(result);
+                Iterator iterator = issueObj.keys();
+                while (iterator.hasNext()) {
+                    String key = (String) iterator.next();
+                    JSONObject issue = issueObj.getJSONObject(key);
 
+                    //  get id from  issue
+                    currency = key;
+                    buy_price = issue.optString("buy");
+                    sell_price = issue.optString("sell");
+
+                    System.out.println("Currency: " + currency);
+                    System.out.println("Buy: " + buy_price);
+                    System.out.println("Sell:" + sell_price);
+
+                    api_result.add(new Feedback(buy_price.toString(), key, false));
+                    System.out.println(api_result.toString());
+                }
             } catch (JSONException e) {
                 e.printStackTrace();
             }
 
-            /*
             //Adapter
-            AdapterFeedbacks adapter = new AdapterFeedbacks(list);
+            adapter = new AdapterFeedbacks(api_result);
 
             //Layout Manager
             RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(getApplicationContext());
+            recyclerView = findViewById(R.id.api_view);
             recyclerView.setLayoutManager(layoutManager);
             recyclerView.setHasFixedSize(true);
-            recyclerView.addItemDecoration(new DividerItemDecoration(this, LinearLayout.VERTICAL));
+            //recyclerView.addItemDecoration(new DividerItemDecoration(getParent().getApplicationContext(), LinearLayout.VERTICAL));
             recyclerView.setAdapter(adapter);
-        }*/
         }
     }
 }
