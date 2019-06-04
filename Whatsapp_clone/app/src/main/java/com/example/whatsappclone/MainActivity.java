2e2200143c6feb268208c9d193aa00f85c63b8e4
burca <rlpelizzon@hotmail.com>
2019-06-04 15:29:13 -0300

    Tudo funcionando até o .add(String, Class), que esta dando erro no metodo e não consegue usar a Classe de Fragment criada

--- Whatsapp_clone/app/src/main/java/com/example/whatsappclone/MainActivity.java
+++ Whatsapp_clone/app/src/main/java/com/example/whatsappclone/MainActivity.java
@@ -1,13 +1,15 @@
 package com.example.whatsappclone;
 
-import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuInflater;
 
-import com.example.whatsappclone.activities.LoginActivity;
+import com.example.whatsappclone.fragment.ContatosFragment;
+import com.example.whatsappclone.fragment.ConversasFragment;
+import com.ogaclejapan.smarttablayout.utils.v4.FragmentPagerItemAdapter;
+import com.ogaclejapan.smarttablayout.utils.v4.FragmentPagerItems;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -18,6 +20,13 @@ public class MainActivity extends AppCompatActivity {
 
         Toolbar toolbar = findViewById(R.id.toolbarPrincipal);
         setSupportActionBar(toolbar);
+
+        FragmentPagerItemAdapter adapter = new FragmentPagerItemAdapter(
+                getSupportFragmentManager(),
+                FragmentPagerItems.with(this)
+                .add("Conversas", ConversasFragment.class)
+                //.add("Contatos", ContatosFragment.class)
+                .create());
     }
 
     @Override
