d320af098ccc899838c792b318be9f55543720a6
burca <rlpelizzon@hotmail.com>
2019-06-04 14:31:50 -0300

    tela de login e cadastro completas, tela principal com toolbar e itens de menu feitos e com tema aplicado

--- Whatsapp_clone/app/src/main/java/com/example/whatsappclone/MainActivity.java
+++ Whatsapp_clone/app/src/main/java/com/example/whatsappclone/MainActivity.java
@@ -3,6 +3,9 @@ package com.example.whatsappclone;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuInflater;
 
 import com.example.whatsappclone.activities.LoginActivity;
 
@@ -13,7 +16,16 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Intent intent = new Intent(getBaseContext(), LoginActivity.class);
-        startActivity(intent);
+        Toolbar toolbar = findViewById(R.id.toolbarPrincipal);
+        setSupportActionBar(toolbar);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_main, menu);
+
+        return super.onCreateOptionsMenu(menu);
     }
 }
--- Whatsapp_clone/app/src/main/java/com/example/whatsappclone/activities/LoginActivity.java
+++ Whatsapp_clone/app/src/main/java/com/example/whatsappclone/activities/LoginActivity.java
@@ -1,8 +1,11 @@
 package com.example.whatsappclone.activities;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
+import com.example.whatsappclone.MainActivity;
 import com.example.whatsappclone.R;
 
 public class LoginActivity extends AppCompatActivity {
@@ -11,5 +14,17 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+    }
+
+    public void abrirTelaCadastro(View view)
+    {
+        Intent intent = new Intent(getBaseContext(), CadastroAcitivity.class);
+        startActivity(intent);
+    }
+
+    public void abrirTelaPrincipal(View view)
+    {
+        Intent intent = new Intent(getBaseContext(), MainActivity.class);
+        startActivity(intent);
     }
 }
