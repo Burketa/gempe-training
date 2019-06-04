8940a185d4bd8915ef6edb7be92632e3cf1b4090
burca <rlpelizzon@hotmail.com>
2019-06-04 09:38:14 -0300

    interface tela de login feita

--- Whatsapp_clone/app/src/main/java/com/example/whatsappclone/MainActivity.java
+++ Whatsapp_clone/app/src/main/java/com/example/whatsappclone/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.whatsappclone;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.example.whatsappclone.activities.LoginActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Intent intent = new Intent(getBaseContext(), LoginActivity.class);
+        startActivity(intent);
     }
 }
