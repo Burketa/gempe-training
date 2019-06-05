547979a404b639f72e0f818d585e2510698bd109
burca <rlpelizzon@hotmail.com>
2019-06-05 11:40:59 -0300

    Tela de chat feita mas sem lista de chats fixos ainda

--- Whatsapp_clone/app/src/main/java/com/example/whatsappclone/fragment/ContatosFragment.java
+++ Whatsapp_clone/app/src/main/java/com/example/whatsappclone/fragment/ContatosFragment.java
@@ -1,6 +1,7 @@
 package com.example.whatsappclone.fragment;
 
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.LinearLayoutManager;
@@ -8,9 +9,12 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 
 import com.example.whatsappclone.R;
+import com.example.whatsappclone.activities.ChatActivity;
 import com.example.whatsappclone.adapter.ContatosAdapter;
+import com.example.whatsappclone.helper.RecyclerItemClickListener;
 import com.example.whatsappclone.model.Usuario;
 
 import java.util.ArrayList;
@@ -40,9 +44,9 @@ public class ContatosFragment extends Fragment {
         recyclerViewListaContatos = view.findViewById(R.id.recyclerViewListaContatos);
 
         //Popular lista de contatos estatica
-        listaContatos.add(new Usuario("Rafael", "rafael@gempe.com"));
-        listaContatos.add(new Usuario("Fernanda", "fernanda@gempe.com"));
-        listaContatos.add(new Usuario("Geovanne", "geovanne@gempe.com"));
+        listaContatos.add(new Usuario("1", "Rafael", "rafael@gempe.com"));
+        listaContatos.add(new Usuario("2", "Fernanda", "fernanda@gempe.com"));
+        listaContatos.add(new Usuario("3", "Geovanne", "geovanne@gempe.com"));
 
 
         //configurar adapter
@@ -53,6 +57,40 @@ public class ContatosFragment extends Fragment {
         recyclerViewListaContatos.setLayoutManager( layoutManager );
         recyclerViewListaContatos.setHasFixedSize( true );
         recyclerViewListaContatos.setAdapter( adapter );
+
+        recyclerViewListaContatos.addOnItemTouchListener(
+                new RecyclerItemClickListener(
+                        getActivity(),
+                        recyclerViewListaContatos,
+                        new RecyclerItemClickListener.OnItemClickListener() {
+                            @Override
+                            public void onItemClick(View view, int position) {
+                                //TODO: implementar o que cada item faz quando clicacdo
+                                System.out.println("asd");
+
+                                Intent intent = new Intent(getActivity(), ChatActivity.class);
+                                startActivity(intent);
+
+                                //Toast.makeText(, "clique: ", Toast.LENGTH_SHORT).show();
+                                /*Usuario usuarioSelecionado = listaContatos.get( position );
+                                Intent i = new Intent(getActivity(), ChatActivity.class);
+                                i.putExtra("chatContato", usuarioSelecionado );
+                                startActivity( i );*/
+
+                            }
+
+                            @Override
+                            public void onLongItemClick(View view, int position) {
+
+                            }
+
+                            @Override
+                            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+
+                            }
+                        }
+                )
+        );
 
         return view;
     }
